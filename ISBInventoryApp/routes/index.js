12b5af3fb2c81c69bea5b7e065f24f3da2a3a34d
Nelson Liu <nelson.liu.2009@gmail.com>
2015-04-12 21:54:54 -0700

    linked DB to jade / node

--- ISBInventoryApp/routes/index.js
+++ ISBInventoryApp/routes/index.js
@@ -6,6 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET ISBinventory page. */
+router.get('/ISBinventory', function(req, res) {
+    var db = req.db;
+    var collection = db.get('inventory');
+    collection.find({},{},function(e,docs){
+        res.render('ISBinventory', {
+            "ISBinventory" : docs
+        });
+    });
+});
+
 router.get('/helloworld', function(req, res){
   res.render('helloworld', {title: 'Hello, World!'})
 });
