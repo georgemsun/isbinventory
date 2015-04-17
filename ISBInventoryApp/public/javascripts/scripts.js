b2302789652fc197c501ac392c093a3f65a89da3
Nelson Liu <nelson.liu.2009@gmail.com>
2015-04-16 22:07:32 -0700

    toggle save / edit buttons

--- ISBInventoryApp/public/javascripts/scripts.js
+++ ISBInventoryApp/public/javascripts/scripts.js
@@ -1,8 +1,21 @@
 $(document).ready(function(){
+  var saveIconOn = false;
+
     $(".inventorycell").hover(function(){
-        $(this).find(".editpencil").css("display", "inline-block");
-        },function(){
-        $(this).find(".editpencil").css("display", "none");
+        $(this).find(".saveEditButton").css("display", "inline-block");
+    },function(){
+        $(this).find(".saveEditButton").css("display", "none");
+      });
+
+    $(".saveEditButton").click(function(e){
+      $(this).toggleClass('fa-pencil fa-save');
+      $(this).toggleClass('button-active');
+      if(saveIconOn == true){
+        //POST data to DB
+      }
+      saveIconOn = !saveIconOn;
+      console.log(saveIconOn);
     });
+
 
 });
